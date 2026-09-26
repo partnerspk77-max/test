529c1861e7ad82cb047a28143ea349f2bbe1ff52
meherzain636 <meherzain636@users.noreply.github.com>
2026-09-26 14:45:50 +0500

    Remove all project files

--- lines/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Transit Line Extractors Registry"""
-from .base import BaseTransitExtractor
-from .mrt_kajang import MrtKajangExtractor
-
-EXTRACTORS = {
-    "mrt-kajang": MrtKajangExtractor,
-}
-
-__all__ = ["BaseTransitExtractor", "MrtKajangExtractor", "EXTRACTORS"]
